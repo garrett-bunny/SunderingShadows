0bf4117536a3d0e04826082e27e6336b797ef835
Yves <mana.starved@yahoo.com>
2021-10-04 11:38:33 -0700

    Remove target check that was causing it to fail

--- cmds/spells/w/_wings_of_darkness.c
+++ cmds/spells/w/_wings_of_darkness.c
@@ -1,8 +1,8 @@
 /*
   _wings_of_darkness.c
-  
+
   Shadow mystery innate power.
-  
+
   -- Tlaloc --
 */
 
@@ -34,25 +34,25 @@ string query_cast_string()
 }
 
 void spell_effect(int prof)
-{   
-    if(!objectp(caster) || !objectp(target))
+{
+    if(!objectp(caster))
         return;
-    
+
     if(caster->query_property("flying"))
     {
         tell_object(caster,"You're already under a similar effect!");
         this_object()->remove();
         return;
     }
-    
+
     spell_successful();
-    
-    caster->set_property("added short",({"%^BOLD%^%^CYAN%^ (trailed by %^BLACK%^%^BOLD%^inky black wings%^CYAN%^)%^RESET%^"}));    
+
+    caster->set_property("added short",({"%^BOLD%^%^CYAN%^ (trailed by %^BLACK%^%^BOLD%^inky black wings%^CYAN%^)%^RESET%^"}));
     caster->set_property("flying", 1);
-    
+
     tell_object(caster, CRAYON_D->color_string("You draw on the shadows around you to form a set of translucent, inky wings.", "very black"));
     tell_room(place, CRAYON_D->color_string(caster->query_cap_name() + " draws on the shadows around " + caster->query_objective() + " to form a set of translucent, inky wings.", "very black"), caster);
-    
+
     addSpellToCaster();
 }
 
@@ -64,11 +64,6 @@ void dest_effect()
         caster->remove_property_value("added short",({"%^BOLD%^%^CYAN%^ (trailed by %^BLACK%^%^BOLD%^inky black wings%^CYAN%^)%^RESET%^"}));
         caster->remove_property("flying");
     }
-    
+
     ::dest_effect();
 }
-    
-    
-    
-
-    
