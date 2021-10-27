dbbedd232a489e32942d3fd8aebf4c497f6ee6a4
shadowgate <law@shadowgate.org>
2021-10-27 19:30:05 +0000

    Autocommit

--- cmds/system/_updatenew.c
+++ cmds/system/_updatenew.c
@@ -8,8 +8,64 @@
 */
 
 #include <std.h>
+#include <security.h>
 
 inherit DAEMON;
+
+int cmd_updatenew(string str)
+{
+    string file, tmp, *flags;
+    int deep;
+    object ob, *obs;
+    
+    sscanf(str, "%s %s", file, tmp);
+    
+    tell_object(this_player(), "file = " + file);
+    
+    if(strlen(tmp))
+        flags = explode(tmp, " ");
+    
+    if(sizeof(flags))
+    {
+        if(member_array("r", flags) >= 0)
+            deep = 1;
+        if(member_array("R", flags) >= 0)
+            deep = 2;
+        if(member_array("z", flags) >= 0)
+            deep = 3;
+    }
+    
+    if(!file || file == "here")
+        file = "/" + file_name(environment(this_player()));
+    
+    if(file_size(file) == -2 && file_size(file + ".c") < 0)
+    {
+        write("Update : file is a directory.\n");
+        return 1;
+    }
+    
+    obs = ({  });
+    
+    if(ob = find_object(file))
+    {
+        obs = filter_array(all_inventory(ob), (: objectp($1) :));
+        foreach(object obj in obs)
+            obj->move("/d/shadowgate/void");
+    }
+    
+    if(file[<2..<1] != ".c")
+        file += ".c";
+    
+    seteuid(getuid(previous_object()));
+    
+    if(do_update(file, deep) < time())
+        write(file + " : No update necessary.");
+    
+    seteuid(UID_SYSTEM);
+    
+    foreach(object obj in obs)
+        obj->move(ob);
+}
 
 int do_update(string file, int deep)
 {
@@ -47,8 +103,11 @@ int do_update(string file, int deep)
         
         if(ob)
         {
-            foreach(string fn in inherit_list(ob))
-                do_update(fn, deep > 1 ? deep : 0);
+            if(deep)
+            {
+                foreach(string fn in inherit_list(ob))
+                    do_update(fn, deep > 1 ? deep : 0);
+            }
                 
             destruct(ob, 1);
         }
@@ -58,7 +117,9 @@ int do_update(string file, int deep)
     if(file[0] != '/')
         file = "/" + file;
     
-    write(file + " : updated and loaded.\n");
+    write(file + " : updated and loaded.");
     
     return time();
-}   
+}
+
+   
