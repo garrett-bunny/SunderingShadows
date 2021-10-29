6331e5a1f1e06a4233df15acfda6c2404440019e
shadowgate <law@shadowgate.org>
2021-10-29 00:00:04 +0000

    Autocommit

--- d/islands/tonerra/mountain/crevice.c
+++ d/islands/tonerra/mountain/crevice.c
@@ -8,7 +8,7 @@ void create(){
 
   set_no_clean(1);
    set_name("crevise");
-   set_short("A crevise in the wall");
+   set_short("A crevice in the wall");
    set_long("%^C202%^This is a crevise in the wall here, the ground here is stable and offers protection from the depths "
 			"below. The %^C100%^walls %^C202%^are rough and covered with a sparse %^C149%^vegetation%^C202%^, clinging for"
 			"life in a mostly inhospitable location.\n");
