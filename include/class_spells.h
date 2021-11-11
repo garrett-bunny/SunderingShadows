941a176e0e9e144e5205009f8e92c1f9cabcbdf3
shadowgate <law@shadowgate.org>
2021-11-11 10:30:03 +0000

    Autocommit

--- include/class_spells.h
+++ include/class_spells.h
@@ -35,7 +35,7 @@ class_spell_storage =
     //"whimsy":
     //    ({"faerie fire", "hideous laughter", "phantom steed", "confusion", "false vision", "eyebite", "project image", "irresistible dance", "weird"}),
     "heavens":
-        ({"color spray", "hypnotic pattern", "daylight", "rainbow pattern", "angelic aspect", "spray of shooting stars", "prismatic spray", "sunburst", "meteor swarm"}),
+        ({"color spray", "hypnotic pattern", "daylight", "rainbow pattern", "umbral strike", "spray of shooting stars", "prismatic spray", "sunburst", "meteor swarm"}),
     "life":
         ({"celestial healing", "restoration", "remove blindness", "neutralize poison", "breath of life", "heal", "regenerate", "mass heal", "resurrection"}),
     "shadow":
