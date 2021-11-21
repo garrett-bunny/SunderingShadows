4de7febced3bd57e9f76ca5af7fa0f8c04ba7936
shadowgate <law@shadowgate.org>
2021-11-21 15:30:02 +0000

    Autocommit

--- cmds/spells/s/_scintillating_pattern.c
+++ cmds/spells/s/_scintillating_pattern.c
@@ -13,31 +13,49 @@ void create()
     set_spell_level(([ "mage" : 8, "cleric" : 8 ]));
     set_spell_sphere("illusion");
     set_domains("illusion");
+    set_save("will");
     set_syntax("cast CLASS scintillating pattern");
-    set_damage_desc("mass confusion for clevel/5 + 1 rounds on enemies");
-    set_description("This casting summons forward illusionary patter that briefly captivates and confuses enemies that are weaker than the spell.");
-    splash_spell(2);
+    set_damage_desc("mass confusion, or staggered on save");
+    set_description("This casting summons forward illusionary patterns that attempt to confuse all targets in the area. If they make a will save, they are instead staggered.");
+    splash_spell(3);
 }
 
 void spell_effect(int prof)
 {
     object* attackers;
     int i, admg;
+    
     attackers = target_selector();
 
     tell_room(place, "%^MAGENTA%^%^BOLD%^Spirals of scintillating patterns appear in the air!%^RESET%^");
-    if (!sizeof(attackers)) {
+    
+    if (!sizeof(attackers))
+    {
+        tell_object(caster, "There is no one here.");
         dest_effect();
         return;
     }
-
-    for (i = 0; i < sizeof(attackers); i++) {
-        if (!objectp(attackers[i])) {
+    
+    foreach(object ob in attackers)
+    {
+        if(!objectp(ob))
+            continue;
+        
+        if((ob->query_level() < clevel || !do_save(ob, 0)) && !mind_immunity_damage(ob))
+        {
+            tell_room(place, "%^MAGENTA%^The patterns captivate " + ob->query_cap_name() + "!", ob);
+            tell_object(ob, "%^MAGENTA%^You are captivated by the scintillating pattern, and lash out!%^RESET%^");
+            "/std/effect/status/confused"->apply_effect(ob, clevel / 5 + 1);
             continue;
         }
-        if (attackers[i]->query_level() < clevel && !mind_immunity_damage(attackers[i])) {
-            tell_room(place, "%^MAGENTA%^The patterns captivate " + attackers[i]->QCN + "!");
-            "/std/effect/status/confused"->apply_effect(target, clevel / 5 + 1);
+        else
+        {
+            tell_room(place, "%^BOLD%^" + ob->query_cap_name() + " is shocked by the patterns!%^RESET%^", target);
+            tell_object(ob, "%^BOLD%^You are shocked by the scintillating patterns!%^RESET%^");
+            "/std/effect/status/staggered"->apply_effect(ob, clevel / 5 + 1);
+            if(!userp(ob))
+                spell_kill(ob, caster);
+            continue;
         }
     }
     spell_successful();
