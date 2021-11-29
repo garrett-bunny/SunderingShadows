0ec201e47823c391a66ceabd737f93e1720ad6f6
shadowgate <law@shadowgate.org>
2021-11-29 13:30:02 +0000

    Autocommit

--- d/dagger/ogres/obj/talos_greaves.c
+++ d/dagger/ogres/obj/talos_greaves.c
@@ -4,21 +4,21 @@ inherit "/d/common/obj/armour/greaves.c";
 
 void create(){
     ::create();
-    set_name("obsidion greaves");
-    set_id(({"greaves","obsidion greaves","Greaves","armor"}));
-    set_obvious_short("%^BLACK%^%^BOLD%^obsidion greaves%^RESET%^");
+    set_name("obsidian greaves");
+    set_id(({"greaves","obsidian greaves","Greaves","armor"}));
+    set_obvious_short("%^BLACK%^%^BOLD%^obsidian greaves%^RESET%^");
     set_short("%^RESET%^%^BOLD%^%^BLACK%^Re%^RESET%^%^MAGENTA%^b%^RESET%^"+
 "%^BOLD%^%^BLACK%^elli%^RESET%^%^MAGENTA%^o%^RESET%^%^BOLD%^%^BLACK%^n's "+
 "%^RESET%^%^MAGENTA%^S%^RESET%^%^BOLD%^%^BLACK%^tep%^RESET%^");
     set_long("%^RESET%^%^BOLD%^%^BLACK%^These greaves have been formed "+
-"from solid %^RESET%^%^WHITE%^obsidion%^RESET%^%^BOLD%^%^BLACK%^, harde"+
+"from solid %^RESET%^%^WHITE%^obsidian%^RESET%^%^BOLD%^%^BLACK%^, harde"+
 "ned through some magic until it is as hard as %^RESET%^%^WHITE%^steel"+
 "%^RESET%^%^BOLD%^%^BLACK%^. The front is decorated with an inlay of "+
 "%^RESET%^%^RED%^re%^RESET%^%^BOLD%^%^RED%^d%^RESET%^%^RED%^ agate "+
 "%^RESET%^%^BOLD%^%^BLACK%^and %^RESET%^%^ORANGE%^p%^RESET%^%^YELLOW%^"+
 "y%^RESET%^%^ORANGE%^rite %^RESET%^%^BOLD%^%^BLACK%^into a design of a"+
 " twisting storm. The workmanship of the design is perfect, and even "+
-"though the greaves seem used, there isnt a scratch on them. They buckle"+
+"though the greaves seem used, there isn't a scratch on them. They buckle"+
 " with a series of %^RESET%^%^ORANGE%^leather straps %^RESET%^%^BOLD%^"+
 "%^BLACK%^that tie behind the leg.\n%^RESET%^");
      set_lore("%^RESET%^%^MAGENTA%^A favorite of Templars of Nilith, the"+
