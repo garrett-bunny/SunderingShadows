c5c916d6e85c7df175c4fa74431a1e9a922bc1de
shadowgate <law@shadowgate.org>
2021-12-01 16:30:02 +0000

    Autocommit

--- std/living.c
+++ std/living.c
@@ -246,11 +246,24 @@ void set_parrying(int i)
     parrying = i;
 }
 
-int query_parrying()
+int query_parrying(object att)
 {
-    object* weapons;
+    object* weapons, *eweapons;
     weapons = TO->query_wielded();
     if (sizeof(weapons) && !weapons[0]->is_lrweapon()) {
+        
+        eweapons = att->query_wielded();
+        
+        //can't parry if both weapons ranged
+        if(sizeof(eweapons) && eweapons[0]->is_lrweapon())
+        {
+            if(sizeof(eweapons) == 1)
+                return 0;
+            
+            if(sizeof(eweapons) > 1 && eweapons[1]->is_lrweapon())
+                return 0;
+        }
+            
         if (FEATS_D->usable_feat(TO, "parry")) {
             return 1;
         }
