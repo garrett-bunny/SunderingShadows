a3d64805e7fecd0e9bb37acd95154f014f382f39
shadowgate <law@shadowgate.org>
2021-12-06 05:00:03 +0000

    Autocommit

--- d/islands/serakii/rooms/village/item.c
+++ d/islands/serakii/rooms/village/item.c
@@ -14,7 +14,7 @@ void create() {
     set_long("%^C094%^This shop is a small %^C244%^cramped %^C094%^building lit by a %^C255%^single candle%^C094%^ on the %^C058%^counter%^C094%^. There are %^C058%^shelves %^C094%^lining every wall and are stocked with %^C113%^items%^C094%^. The construction is simple, rustic, and cozy, yet there is also an air of %^C001%^poverty %^C094%^here. There are more empty spots on the shelves than full.\n%^CRST%^");
     set_exits(([
 
-          "north" : TOWN"9",
+          "north" : TOWN"8",
 	]));
 
      set_items(([
