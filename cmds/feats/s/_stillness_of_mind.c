05ba41c4eefe1d31ae75f6ac1afe83f55ed316d6
shadowgate <law@shadowgate.org>
2021-12-08 21:30:03 +0000

    Autocommit

--- cmds/feats/s/_stillness_of_mind.c
+++ cmds/feats/s/_stillness_of_mind.c
@@ -136,8 +136,10 @@ void execute_feat()
         "intently and you watch as "+caster->QS+" envisions himself free to "+
         "move at will!%^RESET%^", caster);
     }
-    caster->set_tripped(0);
-    caster->set_paralyzed(0);
+    //caster->set_tripped(0);
+    //caster->set_paralyzed(0);
+	ETO->remove_paralyzed();
+    ETO->set_tripped(0);
     delay_messid_msg(90,"%^BOLD%^%^WHITE%^You can %^CYAN%^stillness of mind%^WHITE%^ again.%^RESET%^");
     caster->remove_property("using stillness of mind");
     caster->set_property("using stillness of mind", time());
