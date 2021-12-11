3582dfe2a70bdae63ff5566d442a3453116656aa
shadowgate <law@shadowgate.org>
2021-12-11 22:30:02 +0000

    Autocommit

--- cmds/feats/d/_dazzling_display.c
+++ cmds/feats/d/_dazzling_display.c
@@ -47,20 +47,20 @@ void execute_feat() {
 	tell_object(caster,"%^C107%^You take a deep breath and prepare yourself for your dazzling display.%^C107%^");
 	caster->set_property("dazzling_display", 1);
     caster->add_cooldown("dazzling_display", 60); 
-	call_out("mid_display", 2); 
+	call_out("mid_display", 6); 
 
 }
 
 void mid_display(){
 	
 	tell_room(place,"%^C107%^" + caster->query_cap_name() + " begins to dance and twirl as they display and an awesome display of control. With an abrupt stop they end in an intimidating stance, facing you with a small smile.",({target}));
-	tell_object(caster,"%^C107%^You begin your dance, ensuring every moment is visible to " + target->query_cap_name() + ", wanting to ensure that they fully appreciate the skill you hold.");
+	tell_object(caster,"%^C107%^You begin your dance, ensuring every moment is visible to your enemies to ensure that they fully appreciate the skill you hold.");
 	caster->use_stamina(roll_dice(2,6));
 	caster->set_property("using instant feat", 1);
-	call_out("finish_display", 2); 
+	call_out("execute_attack", 12); 
 }
 	
-void finish_display() {
+void execute_attack() {
 	object *foes=({}),target;
 	int i, damage, mod;
 	
