d10b59be5044e250289af1a2bfc9d06a63d70cbc
shadowgate <law@shadowgate.org>
2021-12-11 03:00:02 +0000

    Autocommit

--- d/antioch/jarmila/obj/divinewrath.c
+++ d/antioch/jarmila/obj/divinewrath.c
@@ -2,6 +2,8 @@
 
 #include <std.h>
 #include <daemons.h>
+#include <teleport.h>
+
 inherit "/d/common/obj/weapon/giant_bastard.c";
 
 
@@ -48,6 +50,122 @@ void create()
     set_wield((:TO,"wield_func":));
     set_unwield((:TO,"unwield_func":));
     set_hit((:TO,"extra_hit":));
+}
+
+init()
+{
+    ::init();
+    add_action("plane_slice", "slice");
+}
+
+string color_me(string str) { return  str = CRAYON_D->color_string(str, "sierra"); }
+
+int plane_slice(string arg)
+{
+    string destination, *destination_names = ({}), destination_file;
+    mapping locations = ([]);
+    object destination_room;
+    int power, bonus;
+    
+    if(!arg)
+    {
+        tell_object(this_player(),"You need to specify a destination");
+        return 1;
+    }
+    
+    if(this_player()->query_current_attacker())
+    {
+        tell_object(this_player(), "You are a little busy for that right now!");
+        return 1;
+    }
+
+    if(this_player()->query_bound() || this_player()->query_tripped() || this_player()->query_paralyzed())
+    {
+        this_player()->send_paralyzed_message("info",TP);
+        return 1;
+    }
+    
+    if(this_player()->cooldown("plane slice"))
+    {
+        tell_object(this_player(), "You're not ready to slice the planes yet.");
+        return 1;
+    }
+
+    locations = this_player()->query_rem_rooms();
+    if(!mapp(locations) || !sizeof(keys(locations)))
+    {
+        tell_object(this_player(),"You don't seem to have any locations remembered");
+        return 1;
+    }
+
+    destination_names = keys(locations);
+    destination = arg;
+
+    if(member_array(destination, destination_names) == -1)
+    {
+        tell_object(this_player(),"You don't have a location named "+destination+" remembered");
+        return 1;
+    }
+
+    destination_file = locations[destination];
+
+    if(catch(call_other(destination_file,"??")))
+    {
+        tell_object(this_player(),"There seems to be an error with the destination room.");
+        return 1;
+    }
+
+    destination_room = find_object_or_load(destination_file);
+
+    if(!objectp(destination_room) || !destination_room->is_room())
+    {
+        tell_object(this_player(), "Your destination doesn't seem to be a room.");
+        return 1;
+    }
+
+    if(!TELEPORT->object_can_be_teleported(this_player(),destination_room, this_player()->query_level() + roll_dice(1, 20)))
+    {
+        tell_object(this_player(),"You notice some kind of interference preventing you from shadow jumping.");
+        return 1;
+    }
+
+    this_player()->clear_followers();
+    this_player()->setAdminBlock();
+
+    tell_object(this_player(),color_me("You heft your blade in a mighty swing, slicing through the very fabric of reality."));
+    tell_object(this_player(),color_me("The blade makes a clean slice and a rift appears in the air in front of you. You step through to another place..."));
+
+    tell_room(destination_room,"A vertical slice appears in the air and opens downward, revealing another place beyond.",this_player());
+
+    call_out("move_caster",1, destination_room, this_player());
+    return 1;
+}
+
+void move_caster(object dest, object player)
+{
+    if(!objectp(player))
+        return;
+
+    if(!objectp(dest))
+    {
+        tell_object(player,color_me("Something has gone wrong with the destination.."));
+        tell_object(player,color_me("The rift in reality closes itself up."));
+        tell_object(player,color_me("You remain where you were standing."));
+        tell_room(dest,"There's a slight flicker in the air.",player);
+        return;
+    }
+
+    player->move(dest);
+    player->removeAdminBlock();
+    player->force_me("look");
+    tell_object(player,color_me("You can feel a rush of sensation you step through the rift to your destination."));
+    tell_object(player,color_me("You step silently from the rift."));
+    tell_room(dest, player->query_cap_name() + " steps silently from the rift.", player);
+    tell_object(player,color_me("You have arrived at your destination."));
+    this_player()->add_cooldown("plane slice", 60);
+
+    dest_effect();
+    return;
 }
 
 int wield_func()
