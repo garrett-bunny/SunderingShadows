6ba65c3532cf36aeb8ca13cb1f3e7fe5403a1265
shadowgate <law@shadowgate.org>
2021-12-12 02:00:03 +0000

    Autocommit

---
+++ cmds/feats/a/_advanced_training.c
@@ -0,0 +1,54 @@
+#include <std.h>
+#include <daemons.h>
+inherit FEAT;
+
+void create()
+{
+    ::create();
+    feat_type("permanent");
+    feat_category("AdvancedTraining");
+    feat_name("advanced training");
+    feat_prereq("Fighter L21");
+    feat_desc("Dedicated fighters are known for their peerless training in martial pursuits. With this feat, the fighter unlocks a group of Advanced Training feats to choose from. In addition, the fighter gains an additional 10% damage with all weapons.");
+    permanent(1);
+    set_required_for(({ "abundant tactics", "fighter finesse", "fighter reflexes", "aura of confidence", "defensive weapon training", "armored juggernaut", "master armorsmith", "master weaponsmith", "unmoving" }));
+}
+
+int prerequisites(object ob) 
+{
+    if(!objectp(ob)) { return 0; }
+
+    if((int)ob->query_class_level("fighter") < 21)
+    {
+        dest_effect();
+        return 0;
+    }
+}
+
+void execute_feat() 
+{
+    ::execute_feat();
+    dest_effect();
+}
+
+void permanent_effects(object ob)
+{
+    ::permanent_effects(ob);
+    dest_effect();
+    return;
+}
+
+void reverse_permanent_effects(object ob)
+{
+    ::reverse_permanent_effects(ob);
+    dest_effect();
+    return;
+}
+
+void dest_effect()
+{
+    ::dest_effect();
+    remove_feat(TO);
+    return;
+}
+
--- d/islands/serakii/storage/fallow_field.c
+++ d/islands/serakii/storage/fallow_field.c
@@ -20,7 +20,7 @@ void create() {
         set_long("blah");
         switch(seas){
         case "summer": 
-            set_long("s%^C168%^These %^C222%^fields%^C168%^ at one point were planted with %^C220%^corn%^C168%^ however it looks like for several seasons the fields have %^C249%^lay fallow%^C168%^. %^C094%^Mud%^C168%^ has churned up the %^C095%^soil%^C168%^ and the remains of %^C220%^corn cobs%^C168%^ and %^C220%^stalks%^C168%^ are everywhere. In the %^C125%^heat%^C168%^ of the season everything has %^C058%^dried%^C168%^ to a %^C058%^crisp%^C168%^. To the east you can see a %^C029%^forest%^C168%^ shrouded in %^C249%^fog%^C168%^ and a %^C246%^giant boulder%^C168%^. Far to the north you can see %^C251%^three fortresses%^C168%^.%^CRST%^\n");
+            set_long("%^C168%^These %^C222%^fields%^C168%^ at one point were planted with %^C220%^corn%^C168%^ however it looks like for several seasons the fields have %^C249%^lay fallow%^C168%^. %^C094%^Mud%^C168%^ has churned up the %^C095%^soil%^C168%^ and the remains of %^C220%^corn cobs%^C168%^ and %^C220%^stalks%^C168%^ are everywhere. In the %^C125%^heat%^C168%^ of the season everything has %^C058%^dried%^C168%^ to a %^C058%^crisp%^C168%^. To the east you can see a %^C029%^forest%^C168%^ shrouded in %^C249%^fog%^C168%^ and a %^C246%^giant boulder%^C168%^. Far to the north you can see %^C251%^three fortresses%^C168%^.%^CRST%^\n");
             set_smell("default","%^C060%^You smell %^C095%^mud%^C060%^ and %^C228%^straw%^CRST%^");
             set_listen("default","%^C140%^You hear strange sounds coming from the %^C028%^forest%^C140%^ to the east%^CRST%^");
         break;
@@ -38,7 +38,7 @@ void create() {
         break;
 
         case "autumn":
-            set_long("s%^C131%^These %^C220%^fields%^C131%^ at one point were planted with %^C220%^corn%^C131%^ however it looks like for several seasons the fields have %^C248%^lay fallow%^C131%^. While the %^C220%^fields%^C131%^ haven't been planted properly, %^C220%^corn%^C131%^ still grows at wild angles and %^C088%^fallen leaves%^C131%^ line everywhere. To the east you can see a %^C029%^forest%^C131%^ shrouded in %^C247%^fog%^C131%^ and a %^C248%^giant boulder%^C131%^. Far to the north you can see %^C248%^three fortresses%^C131%^. %^CRST%^\n");
+            set_long("%^C131%^These %^C220%^fields%^C131%^ at one point were planted with %^C220%^corn%^C131%^ however it looks like for several seasons the fields have %^C248%^lay fallow%^C131%^. While the %^C220%^fields%^C131%^ haven't been planted properly, %^C220%^corn%^C131%^ still grows at wild angles and %^C088%^fallen leaves%^C131%^ line everywhere. To the east you can see a %^C029%^forest%^C131%^ shrouded in %^C247%^fog%^C131%^ and a %^C248%^giant boulder%^C131%^. Far to the north you can see %^C248%^three fortresses%^C131%^. %^CRST%^\n");
             set_smell("default","%^C060%^You smell %^C095%^mud%^C060%^ and %^C228%^straw%^CRST%^.");
             set_listen("default","%^C140%^You hear strange sounds coming from the %^C028%^forest%^C140%^ to the east%^CRST%^");
         break;
