1b38f60139a41628e501aa35fcb20df729936c7c
shadowgate <law@shadowgate.org>
2021-12-13 04:00:02 +0000

    Autocommit

--- cmds/feats/d/_dazzling_display.c
+++ cmds/feats/d/_dazzling_display.c
@@ -12,7 +12,6 @@ void create() {
     feat_prereq("Weapon Focus");
 	feat_syntax("dazzling_display");
     feat_desc("While wielding the weapon in which you have Weapon Focus, you can perform a bewildering show of prowess as a full-round action. Make an Intimidate check to demoralize all foes within 30 feet who can see your display.");
-    set_save("will");
 }
 
 int prerequisites(object ob) {
@@ -44,12 +43,12 @@ void execute_feat() {
     dest_effect();
 	return 1; }
 	
-	tell_object(target,"%^C107%^" + caster->query_cap_name() + " begins to dance and twirl as they display and an awesome display of control. With an abrupt stop they end in an intimidating stance, facing you with a small smile.",({target}));
+	tell_object(target,"%^C107%^" + caster->query_cap_name() + " begins to dance and twirl as they show an awesome display of control. With an abrupt stop they end in an intimidating stance, facing you with a small smile.",({target}));
 	tell_object(caster,"%^C107%^You begin your dance, ensuring every moment is visible to your enemies to ensure that they fully appreciate the skill you hold.");
 	caster->use_stamina(roll_dice(2,6));
 	caster->set_property("using instant feat", 1);
 	caster->set_property("dazzling_display", 1);
-    caster->add_cooldown("dazzling display", 20); 
+    caster->add_cooldown("dazzling display", 60); 
 
 }
 	
@@ -95,7 +94,7 @@ void execute_attack() {
             continue;
         }
 
-        if(do_save(target)) {
+		if(BONUS_D->intimidate_check(target, caster)) {
 		tell_object(caster,"You finish your dance, hoping for the best.");
 		tell_object(target,"%^C107%^You watch the weapons display with keen interest, however, it fails to inspire much %^C194%^fear at all in you.%^C107%^"); 
 		    continue;
