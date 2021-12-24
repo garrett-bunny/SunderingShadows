f96efcc0795b687b5ab34966a4b4e791fa2a31c1
shadowgate <law@shadowgate.org>
2021-12-24 16:30:03 +0000

    Autocommit

---
+++ d/islands/serakii/rooms/ruin/6.c
@@ -0,0 +1,20 @@
+#include <std.h>
+#include "../../serakii.h"
+
+inherit STORAGE"tree_lined.c";
+
+void create(){
+   ::create();
+
+
+   set_exits(([
+
+         // "north" : CAER"1",
+        "south" : RUINS"5",
+         "east" : RUINS"9",
+          "west" : RUINS"7",
+          "east" : RUINS"20",
+
+   ]));
+
+}
