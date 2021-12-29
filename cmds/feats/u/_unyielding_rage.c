af6d1cd8124d35c4bb891efdcc23d3a5d2d1cb01
shadowgate <law@shadowgate.org>
2021-12-29 01:30:03 +0000

    Autocommit

--- cmds/feats/u/_unyielding_rage.c
+++ cmds/feats/u/_unyielding_rage.c
@@ -9,7 +9,7 @@ void create()
    feat_category("RagePower");
    feat_name("unyielding rage");
    feat_prereq("Barbarian L20");
-   feat_desc("This feat allows the barbarian to avoid death's door whilst raging. After avoiding mortal death the barbarian will be unable to do so again for some time.");
+   feat_desc("This feat allows the barbarian to avoid death's door whilst raging. After avoiding mortal death the barbarian will be unable to do so again for some time. Half-orcs already have Orc Ferocity and thus cannot take this feat.");
    permanent(1);
    allow_blind(1);
    set_required_for(({}));
@@ -23,6 +23,13 @@ int prerequisites(object ob)
         dest_effect();
         return 0;
     }
+    
+    if(ob->query_race() == "half-orc")
+    {
+        dest_effect();
+        return 0;
+    }
+    
     return ::prerequisites(ob);
 }
 
