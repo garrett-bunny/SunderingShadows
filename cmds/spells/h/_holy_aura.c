14b444f9c125a5dea1c65ac817a9073a6295aa18
SunderingYves <80606545+SunderingYves@users.noreply.github.com>
2021-12-30 14:19:23 -0800

    Update _holy_aura.c

--- cmds/spells/h/_holy_aura.c
+++ cmds/spells/h/_holy_aura.c
@@ -102,7 +102,6 @@ void execute_attack(){
         tell_room(place,"%^BOLD%^%^WHITE%^The holy light around "+caster->QCN+" falls upon "+caster->QP+" enemies!",({caster,target}));
         tell_object(caster,"%^BOLD%^%^WHITE%^The holy light around you falls upon your enemies!");
         for(i=0;i<sizeof(attackers);i++){
-            if(SAVING_D->saving_throw(attackers[i],"spell",0)) { continue; }
             tell_object(attackers[i],"%^BOLD%^%^WHITE%^You are scorched by the holy light as you strike "
                         ""+caster->QCN+"!");
             damage_targ(attackers[i],attackers[i]->return_target_limb(),sdamage,"divine");
