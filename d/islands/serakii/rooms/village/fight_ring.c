cd78c0bd0919a1e7014ae96489840b22acd4632a
shadowgate <law@shadowgate.org>
2022-01-01 00:00:02 +0000

    Autocommit

--- d/islands/serakii/rooms/village/fight_ring.c
+++ d/islands/serakii/rooms/village/fight_ring.c
@@ -1,14 +1,30 @@
+// Serakii Sparring Pit - LoKi 2022
+
 #include <std.h>
 #include "../../serakii.h"
 
-inherit STORAGE"construction.c";
+inherit CROOM;
 
 void create(){
    ::create();
+    set_property("indoors",1);
+    set_name("sparring pit");
+    set_short("%^C136%^Sparring Pit%^CRST%^");
+set_long("%^C107%^This room has a large cylindrical indention dug into the floor approximately six feet in depth. There are small stairs on either side of the pit for access which are made of solid clay and dirt. The entirety of the room is simply built, with one purpose in mind - fighting. The ground in the pit is soft clay and looks to be designed to minimize impact when fallen on. This room is used for full combat training by the villagers here to practice their training in a more holistic way..%^CRST%^\n");
+
+    set_items(([
+
+    ]));
+
+set_smell("default","%^C064%^You smell incense.%^CRST%^");
+set_listen("default","%^C138%^You hear training around you.%^CRST%^");
+
    set_exits(([
 
           "north" : TOWN"dojo",
-          
+
    ]));
+
+
 
 }
