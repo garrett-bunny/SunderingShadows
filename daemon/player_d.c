f3361617e29499d7d2046a2ff7402d0e773d4b14
shadowgate <law@shadowgate.org>
2022-01-10 17:00:03 +0000

    Autocommit

--- daemon/player_d.c
+++ daemon/player_d.c
@@ -968,3 +968,30 @@ void remove_stun_immunity(object ob)
 {
     ob && ob->remove_property("stun_immunity");
 }
+
+int scry_check(object target, int power)
+{
+    object room;
+    int block_power, room_block;
+    
+    if(!objectp(target) || !power)
+        return 0;
+    
+    room = environment(target);
+    
+    if(!objectp(room))
+        return 0;
+    
+    if(room->query_property("no scry"))
+        return 0;
+    
+    room_block = room->query_property("scry proof");    
+    block_power = target->query_property("scry block power");
+    
+    block_power = max( ({ room_block, block_power }) );   
+   
+    if(block_power > power)
+        return 0;
+    
+    return 1;
+}
