204319e6bff5a02ca8b213cce2ea3ff4b5394fed
SunderingYves <80606545+SunderingYves@users.noreply.github.com>
2022-01-13 09:38:49 -0800

    Update backpack.c

--- d/common/obj/misc/backpack.c
+++ d/common/obj/misc/backpack.c
@@ -8,7 +8,6 @@ void create() {
     set_weight(5);
     set("long", "This is a simple leather backpack that you could put things in.\n");
     set_value(2);
-    set_property("treasure_type", "5A");
     set_max_internal_encumbrance(35);
     set_property("repairtype",({ "leatherwork","tailor" }));
 }
