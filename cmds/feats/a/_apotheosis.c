064a79a7ad40312d769798581dd775028d536081
shadowgate <law@shadowgate.org>
2022-01-14 04:30:03 +0000

    Autocommit

--- cmds/feats/a/_apotheosis.c
+++ cmds/feats/a/_apotheosis.c
@@ -8,7 +8,7 @@ void create() {
     feat_category("DivineSpellcraft");
     feat_name("apotheosis");
     feat_prereq("Cleric L31");
-    feat_desc("The cleric has become the chosen servant of their deity in the world. She becomes a conduit of her deity and thus can cast domain spells at will, without memorization or using spell slots. In addition, she gains +1 caster level on spells of her chosen domain. Finally, the cleric becomes infused with divine energy and so becomes 20% resistant to divine damage.");
+    feat_desc("The cleric has become the chosen servant of their deity in the world. She becomes a conduit of her deity and thus can cast domain spells at will, without using up the memorized spell. In addition, she gains +1 caster level on spells of her chosen domain. Finally, the cleric becomes infused with divine energy and so becomes 20% resistant to divine damage.");
     permanent(1);
     set_required_for(({}));
 }
