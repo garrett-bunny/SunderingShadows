286341e43450f244fbbe3e3f87698b7cd264f0dc
SunderingYves <80606545+SunderingYves@users.noreply.github.com>
2022-01-19 12:45:58 -0800

    Update struck_reviewed.h

--- adm/include/struck_reviewed.h
+++ adm/include/struck_reviewed.h
@@ -54,4 +54,5 @@
     "/d/deku/hhouse/ob/lathanders_dawn", \
     "/d/deku/hhouse/ob/shroud_of_the_impaler", \
     "/d/islands/common/obj/harbinger", \
+    "/d/underdark/lower/dragon/obj/shadowdancer", \
 })
