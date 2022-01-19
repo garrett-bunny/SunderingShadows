3506ba1772efb61feb254133a5d8f2e7630972e8
KismetSuS <80552722+KismetSuS@users.noreply.github.com>
2022-01-19 22:53:56 +0100

    Update road14.c

--- d/dagger/road/road14.c
+++ d/dagger/road/road14.c
@@ -10,13 +10,15 @@ void create(){
     set_property("light", 2);
     set_short("The Dagger Sea coast road.");
     set_long(
-      "%^CYAN%^You are on the Dagger Seacoast road.  To the northwest you can see the emerald hills of the Yniam plains.  To the northeast you can see the seaport of Torm growing large on the horizon.  To the east are the golden beaches of the dagger seacoast."
+      "%^CYAN%^You are on the Dagger Seacoast road. To the northwest you can see %^RESET%^%^GREEN%^the emerald hills%^RESET%^%^CYAN%^ of the Yniam plains. To the northeast you can see the %^BOLD%^%^BLUE%^seaport of Torm%^RESET%^%^CYAN%^ growing large on the horizon. To the east are the %^RESET%^%^ORANGE%^golden beaches%^RESET%^%^CYAN%^ of the dagger seacoast and the %^BOLD%^%^BLACK%^high cliffs%^RESET%^%^CYAN%^ that ring them. From here, you can even just make out a few %^BOLD%^%^BLACK%^caves%^RESET%^%^CYAN%^ in the cliffs that line the southern half of the beach."
     );
     set_smell("default", "You can smell the salts of the sea on the cool breeze.");
     set_listen("default", "The sound of the waves on the cliffs below float up to your ears.");
     set_items( ([
 	"beaches" : "Golden sand lines the beaches along the Dagger Sea.",
 	"sea" : "The great waves crash against teh cliffs hundreds of feet below.",
+	"torm" : "The great seaport of Torm promises rum, fine inns and fresh fish.",
+	"caves" : "It is hard to make out much more from there. You may find a path past the city of Torm.", 
       ]) );
     set_exits( ([
 	"northeast" : RPATH "road15",
