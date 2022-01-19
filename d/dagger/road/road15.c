a2eb2464b6119c9c9c23751f86dcc82aa6ceab4d
KismetSuS <80552722+KismetSuS@users.noreply.github.com>
2022-01-19 23:04:29 +0100

    changing southeast to south

--- d/dagger/road/road15.c
+++ d/dagger/road/road15.c
@@ -10,18 +10,14 @@ void create(){
    set_light(2);
    set_indoors(0);
    set_short("The Dagger Sea coast road");
-   set_long("%^CYAN%^You are very near the seaport of Torm.  You can "
-      "see the city to the northeast, and a little to the north of it "
-      "is Daggerdale, where the road turns inland and away from the cape of "
-      "Gailden and the seaport Torm sitting within it.  To the northeast "
-      "you can still see the emerald Yniam plains.  To the southeast, "
-      "a grand temple stands upon an outcropping along the "
-      "cliffs and overlooking the sea.\n%^RESET%^");
+   set_long("%^CYAN%^You are very near the seaport of Torm, it is just to the northeast. A little to the north of it is the ghost town of Nurval, where the road turns inland and away from the cape of Gailden which Torm sits within. To the northeast you can still see the emerald Yniam plains. To the south a grand temple stands upon an outcropping along the cliffs, overlooking the sea.\n%^RESET%^");
    set_smell("default", "You can smell the salt of the sea on the cool breeze.");
-   set_listen("default", "The sound of the waves on the cliffs below float up to your ears.");
+   set_listen("default", "The waves hit the cliffs far below and within the temple the sounds of people talking and training can be heard.");
    set_items(([
       "beaches" : "Golden sand lines the beaches along the Dagger Sea.",
       "sea" : "The great waves crash against the cliffs hundreds of feet below.",
+      "temple" : "The grand temple of Cevahir stands perched upon the cliff here, crafted from white marble.",
+      "cliff" : "The cliff leads up to the temple and then shoots straight down into the water below.",
    ]));
    set_exits(([
       "northeast" : RPATH "road16",
