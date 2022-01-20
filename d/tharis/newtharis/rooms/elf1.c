6d007152539b479f29a46650d43649db2303f902
shadowgate <law@shadowgate.org>
2022-01-20 19:30:03 +0000

    Autocommit

--- d/tharis/newtharis/rooms/elf1.c
+++ d/tharis/newtharis/rooms/elf1.c
@@ -4,7 +4,7 @@ inherit STORAGE"elf";
 
 void create(){
    ::create();
-   set_long(::query_long()+" A small, abandoned office lies to the south. Further west, the road meanders into the city slums.%^RESET%^\n");
+   set_long(::query_long()+" A small, abandoned office lies to the south. Further west, the road meanders into the city slums. There are signs of fresh construction to the north, as laborers work at assembling scaffolding along incomplete structures.%^RESET%^\n");
    set_exits(([
       "east" : ROOMS"elf2",
       "west" : ROOMS"east3",
