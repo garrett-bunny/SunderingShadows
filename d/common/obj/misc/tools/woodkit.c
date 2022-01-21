9077c7842924d02ad5ba83af099ef813a6b116ea
shadowgate <law@shadowgate.org>
2022-01-21 16:30:03 +0000

    Autocommit

--- d/common/obj/misc/tools/woodkit.c
+++ d/common/obj/misc/tools/woodkit.c
@@ -2,12 +2,12 @@
 inherit OBJECT;
 
 void create(){
-	::create();
-	set_name("woodworkers kit");
-	set_id(({ "kit", "woodworkers kit" }));
+    ::create();
+    set_name("woodworkers kit");
+    set_id(({ "kit", "woodworkers kit" }));
     set_short("%^C058%^woodworkers kit%^CRST%^");
-	set_long("TThis medium sized %^C094%^leather pouch%^CRST%^ is tied with a %^C094%^leather cord%^CRST%^ and opens up into a three panelled storage of a variety of %^C059%^tools%^CRST%^ for %^C095%^woodworking%^CRST%^. While not as exhaustive as a proper woodworking shop, everything is here for quick jobs. There are a variety of different %^C242%^chisels%^CRST%^, %^C142%^small hammers%^CRST%^, and %^C247%^awls%^CRST%^ for working with different types of %^C094%^wood%^CRST%^. A %^C247%^planer%^CRST%^ and different grit levels of %^C143%^sandpaper%^CRST%^ complete the kit. .");
-        set_weight(1);
-	set_value(300);
-         set_item_bonus("academics",2);
+    set_long("TThis medium sized %^C094%^leather pouch%^CRST%^ is tied with a %^C094%^leather cord%^CRST%^ and opens up into a three panelled storage of a variety of %^C059%^tools%^CRST%^ for %^C095%^woodworking%^CRST%^. While not as exhaustive as a proper woodworking shop, everything is here for quick jobs. There are a variety of different %^C242%^chisels%^CRST%^, %^C142%^small hammers%^CRST%^, and %^C247%^awls%^CRST%^ for working with different types of %^C094%^wood%^CRST%^. A %^C247%^planer%^CRST%^ and different grit levels of %^C143%^sandpaper%^CRST%^ complete the kit. .");
+    set_weight(1);
+    set_value(300);
+         set_item_bonus("craft, woodworker",2);
 }
