90e617c9f45beca84c0ceae241ab03444ac0ff83
shadowgate <law@shadowgate.org>
2022-01-30 05:30:03 +0000

    Autocommit

--- d/deku/hhouse/rooms/rt3.c
+++ d/deku/hhouse/rooms/rt3.c
@@ -5,7 +5,7 @@ inherit RT;
 void create() 
 {
     ::create();
-    set_long(TO->query_long() + "%^BOLD%^%^WHITE%^A rickety staircase "+
+    set_long(TO->query_long() + "%^BOLD%^%^WHITE%^ A rickety staircase "+
     "leads down to the second floor of the house. You are on the edge of the "+
     "roof and could %^BOLD%^%^MAGENTA%^jump%^BOLD%^%^WHITE%^ down into "+
     "the courtyard far below if you were so inclined.%^RESET%^");
