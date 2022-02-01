763222ec98c4d3698227dcb0e926d3d82d9757f5
SunderingYves <80606545+SunderingYves@users.noreply.github.com>
2022-02-01 10:44:50 -0800

    Update struck_reviewed.h

--- adm/include/struck_reviewed.h
+++ adm/include/struck_reviewed.h
@@ -7,6 +7,7 @@
     "/d/token_hunt/obj/high/dragonrobe", \
     "/d/token_hunt/obj/low/rainbowbangles", \
     "/d/token_hunt/obj/low/rubytalongaunt", \
+    "/d/token_hunt/obj/mid/chainedovercoat", \
     "/d/token_hunt/obj/mid/paingreaves", \
     "/d/islands/serakii/obj/hound_robe", \
     "/d/islands/serakii/obj/legion_bracers", \
