417bd443b45b07f6a91d826afac3c8e4c03ff664
shadowgate <law@shadowgate.org>
2022-02-11 14:30:02 +0000

    Autocommit

--- cmds/feats/d/_deep_magic.c
+++ cmds/feats/d/_deep_magic.c
@@ -9,7 +9,12 @@ void create() {
     feat_category("ArcaneSpellcraft");
     feat_name("deep magic");
     feat_prereq("Mage L31");
-    feat_desc("Placeholder for testing");
+    feat_desc("The mage has reached a pinnacle of power and understanding in their studies. It is at this point that they begin to delve into the deeper recesses of the weave to grasp power long lost With this feat, the mage gains access to Deep Magic, another type of magic, similar to cantrips or innate. To utilize deep magic spells you have, type use the following syntax.
+    
+    recall deep magic
+    cast deep <spell name>
+
+The mage starts with the wish spell. Any new deep magic must be acquired by finding tomes of knowledge in the world, which will be introduced periodically.");
     permanent(1);
 }
 
