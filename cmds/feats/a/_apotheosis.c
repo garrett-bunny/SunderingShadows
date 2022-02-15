6629a741bdaf0efff143a8b97e118bf735e3fc1a
shadowgate <law@shadowgate.org>
2022-02-15 19:30:03 +0000

    Autocommit

--- cmds/feats/a/_apotheosis.c
+++ cmds/feats/a/_apotheosis.c
@@ -8,7 +8,9 @@ void create() {
     feat_category("DivineSpellcraft");
     feat_name("apotheosis");
     feat_prereq("Cleric L31");
-    feat_desc("The cleric has become the chosen servant of their deity in the world. She becomes a conduit of her deity and thus can cast domain spells at will, without using up the memorized spell. In addition, she gains +1 caster level on spells of her chosen domain. Finally, the cleric becomes infused with divine energy and so becomes 20% resistant to divine damage.");
+    feat_desc("The cleric has become the chosen servant of their deity in the world. She becomes a conduit of her deity and thus can cast domain spells at will, without using up the memorized spell. In addition, she gains +1 caster level on spells of her chosen domain. Finally, the cleric becomes infused with divine energy and so becomes 20% resistant to divine damage.
+    
+%^RED%^BOLD%^RP Note :%^RESET%^ Achieving this level of dedication to your deity means most clerics will take on some physical or emotional traits reflecting the influence of their deity. A cleric of Ashra may have pitch black eyes, while a cleric of Nimnavanon might emit a verdant aura. This is up to your interpretation, and has no actual mechanical bearing, but allows the cleric to express the influence their deity is having on their person.");
     permanent(1);
     set_required_for(({}));
 }
