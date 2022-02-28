b27a6c43736dd7bf20a9468e3893d31c420e54e8
shadowgate <law@shadowgate.org>
2022-02-28 16:30:02 +0000

    Autocommit

--- d/tharis/newtharis/hotel_tharis/obj/cigarette_case.c
+++ d/tharis/newtharis/hotel_tharis/obj/cigarette_case.c
@@ -1,32 +1,74 @@
-// Chernobog (6/8/21)
-// Cigarette Case
+// Chernobog (2/28/22)
+// Updated Cigarette Case
 
 #include <std.h>
 #include <move.h>
-inherit "/std/bag_logic";
+inherit OBJECT;
+
+int charges;
 
 void create(){
     ::create();
     set_id(({"cigarette case","case"}));
     set_name("cigarette case");
-    set_short("%^RESET%^%^MAGENTA%^A %^WHITE%^s%^BOLD%^m%^RESET%^a%^BOLD%^l%^RESET%^l %^BOLD%^%^BLACK%^s%^RESET%^i%^BOLD%^lv%^RESET%^e%^BOLD%^%^BLACK%^r %^RESET%^c%^BOLD%^a%^RESET%^s%^BOLD%^e%^RESET%^");
-    set_long("%^RESET%^%^MAGENTA%^This %^WHITE%^s%^BOLD%^m%^RESET%^a%^BOLD%^l%^RESET%^l c%^BOLD%^a%^RESET%^s%^BOLD%^e %^RESET%^%^MAGENTA%^is shaped from %^WHITE%^l%^BOLD%^um%^RESET%^i%^BOLD%^n%^RESET%^o%^BOLD%^us %^BLACK%^s%^RESET%^i%^BOLD%^lv%^RESET%^e%^BOLD%^%^BLACK%^r%^RESET%^%^MAGENTA%^, the polished surface displaying fine %^BOLD%^%^BLACK%^etchings %^RESET%^%^MAGENTA%^of vines and blooming roses along its periphery. A simple catch snaps the case open for the retrieval or storage of slender cigarettes.%^RESET%^");
+    set_short("%^RESET%^%^CRST%^%^C130%^small %^C244%^s%^C247%^i%^C250%^lv%^C247%^e%^C244%^r %^C130%^case%^CRST%^");
+    set_long("%^RESET%^%^CRST%^%^C130%^This small case is shaped from %^C247%^lu%^C250%^m%^C253%^in%^C250%^o%^C247%^us %^C244%^s%^C247%^i%^C250%^lv%^C247%^e%^C244%^r%^RESET%^%^C130%^, the polished surface displaying %^C243%^f%^C059%^in%^C243%^e e%^C059%^tc%^C243%^hi%^C059%^ng%^C243%^s %^RESET%^%^C130%^of %^C040%^v%^C046%^in%^C040%^e%^C046%^s %^RESET%^%^C130%^and %^C204%^blooming %^C196%^r%^C124%^o%^C196%^se%^C124%^s %^RESET%^%^C130%^along its periphery. A simple catch opens the case in order for you to %^C136%^<retrieve> %^C130%^or %^C136%^<load> %^RESET%^%^C130%^cigarettes.%^CRST%^");
     set_weight(2);
     set_value(5000);
-    set_cointype("gold");
-    set_max_internal_encumbrance(10);
     set_property("repairtype",({ "jeweller" }));
+    charges = 5;
 }
 
-int item_allowed(object item){
-    if(!item->is_cigarette()){
-        write("This case is only meant for cigarettes.");
-        return 0;
+void init(){
+    ::init();
+    add_action("retrieve_fun", "retrieve");
+    add_action("load_fun", "load");
+}
+
+int retrieve_fun(string str){
+    object player, cigarette;
+    player = this_player();
+    
+    if(!str) return 0;
+    if(str != "cigarette") return 0;
+    
+    if(charges < 1){
+        tell_object(player, "%^RESET%^%^CRST%^%^C059%^The cigarette case seems to be empty.%^CRST%^");
+        return 1;
     }
-    if(sizeof(all_inventory(TO)) > 10){
-        write("The case is already full.");
-        return 0;
+    
+    cigarette = new("/d/common/obj/misc/tobacco/cigarette");
+    if((int)(cigarette->move(player)) != MOVE_OK){
+        cigarette->remove();
+        tell_object(player, "%^RESET%^%^CRST%^%^C059%^You have nowhere to put that!%^CRST%^");
+        return 1;
     }
+    tell_object(player, "%^RESET%^%^CRST%^%^C058%^You retrieve a cigarette from your case before snapping it shut.%^CRST%^");
+    tell_room(environment(player), "%^RESET%^%^CRST%^%^C058%^"+player->query_cap_name()+"%^RESET%^%^CRST%^%^C058%^ retrieves a cigarette from "+player->query_possessive()+" case before snapping it shut.%^CRST%^", player);
+    cigarette->move(player);
+    charges--;
+    return 1;
+}
+
+int load_fun(string str){
+    object cigarette, player;
+    player = this_player();
+    
+    if(!str) return 0;
+    if(str != "cigarette") return 0;
+    
+    if(charges > 9){
+        tell_object(player, "%^RESET%^%^CRST%^%^C059%^There is no room for any more cigarettes in the case.%^CRST%^");
+        return 1;
+    }
+    if(!objectp(cigarette = present(str, player))){
+        tell_object(player, "%^RESET%^%^CRST%^%^C059%^You don't have a cigarette to load into the case.%^CRST%^");
+        return 1;
+    }
+    tell_object(player, "%^RESET%^%^CRST%^%^C058%^You load a cigarette into the case.");
+    tell_room(environment(player), "%^RESET%^%^CRST%^%^C058%^"+player->query_cap_name()+"%^RESET%^%^CRST%^%^C058%^ loads a cigarette into "+player->query_possessive()+" case.%^CRST%^", player);
+    cigarette->remove();
+    charges++;
     return 1;
 }
 
