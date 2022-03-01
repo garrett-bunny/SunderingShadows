bc0433eddf0f85cd6deaf56766d1b50681d4a47a
shadowgate <law@shadowgate.org>
2022-03-01 05:30:02 +0000

    Autocommit

--- d/islands/serakii/rooms/village/4.c
+++ d/islands/serakii/rooms/village/4.c
@@ -8,6 +8,7 @@ inherit STORAGE"village.c";
 
 void create(){
    ::create();
+   set_long(::query_long()+"\n%^C058%^There is a squat building to the west.%^CRST%^\n");
    set_exits(([
 
          "west" : TOWN"dojo",
