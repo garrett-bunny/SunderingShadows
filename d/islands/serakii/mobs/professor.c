401827eeaa63f65fa527d54b64554c17895f4906
shadowgate <law@shadowgate.org>
2022-03-04 05:30:02 +0000

    Autocommit

--- d/islands/serakii/mobs/professor.c
+++ d/islands/serakii/mobs/professor.c
@@ -97,19 +97,31 @@ void teach_the_lesson(){
 	
 	switch(prof_type){
 		case "dragons": 
-		    dragon_lesson_1(TP,0);
+		    switch(random(2)) {
+				case 0: 		    dragons_lesson_1(TP,0); break;
+				default: 		    dragons_lesson_2(TP,0); break;
+        }
 		break;
 		
 		case "souls": 
-		    souls_lesson_1(TP,0);
+		    switch(random(2)) {
+				case 0: 		    souls_lesson_1(TP,0); break;
+				default: 		    souls_lesson_2(TP,0); break;
+        }
 		break;
 		
 		case "history": 
-		    history_lesson_1(TP,0);
+		    switch(random(2)) {
+				case 0: 		    history_lesson_1(TP,0); break;
+				default: 		    history_lesson_2(TP,0); break;
+        }
 		break;
 		
 		case "gods": 
-		    gods_lesson_1(TP,0);
+		    switch(random(2)) {
+				case 0: 		    gods_lesson_1(TP,0); break;
+				default: 		    gods_lesson_2(TP,0); break;
+        }
 		break;
 
 		default:
@@ -287,5 +299,170 @@ void gods_lesson_1(object tp,int num){
                 }
     num++;
     call_out("gods_lesson_1",5,tp,num);
+    return;
+}
+
+void souls_lesson_2(object tp,int num){
+            switch(num){
+                case(0):
+                    tell_room(ETP,"%^C152%^The professor steps up to the %^C059%^lectern%^C152%^.%^CRST%^\n");
+					force_me("pose %^C097%^standing at the lectern teaching%^CRST%^");
+                break;
+                case(1):
+		            force_me("say Settle down everyone, please take your seats.");
+                    tell_room(ETP,"%^C144%^The professor shuffles his %^C193%^parchments %^C144%^around and waits.%^CRST%^\n");                    
+                break;
+                case(2):
+                    tell_room(ETP,"%^C075%^students slowly sit on %^C094%^benches %^C075%^and hush.%^CRST%^\n");
+		            force_me("say I am professor "+prof_name+" and we are learning about "+prof_type+ " today.");
+                break;
+                case(3):
+		            force_me("say Specifically we are learning about the Serah Kavall and her banned research. ");
+                    tell_room(ETP,"\n");
+                break;
+                case(4):
+		            force_me("say Kavall was the preeminent expert on collecting and trapping souls, having researched warforged and golems extensively. Unfortunately due to corruption of her research and personal.. weakness. Kavall started working on banned acts.");
+                    tell_room(ETP,"\n");
+                break;
+                case(5):
+		            force_me("say The mass of flesh, named Tears of the Lost.");
+					force_me("emote snides with derision");
+					force_me("say Contains a hundred souls. Remember though, posession of the dagger or creation of more is banned and will result in expulsion.");
+                    tell_room(ETP,"\n");
+                break;
+                case(6):
+				    force_me("say thank you, no questions today, meet me for office hours later.");
+                    tell_room(ETP,"%^C152%^The professor gathers up his papers and steps away from the lectern.%^CRST%^\n");
+                    tell_room(ETP,"%^C152%^The professor gathers up his papers and steps away from the lectern.%^CRST%^\n");
+                    tell_room(ETP,"%^C243%^Another professor walks into the classroom%^CRST%^\n");
+					new(MOBS"professor.c")->move(ETO);
+                    go_home(TO);
+ 
+                break;
+            return;
+                }
+    num++;
+    call_out("souls_lesson_1",5,tp,num);
+    return;
+}
+
+void gods_lesson_2(object tp,int num){
+            switch(num){
+                case(0):
+                    tell_room(ETP,"%^C152%^The professor steps up to the %^C059%^lectern%^C152%^.%^CRST%^\n");
+					force_me("pose %^C097%^standing at the lectern teaching%^CRST%^");
+                break;
+                case(1):
+		            force_me("say Settle down everyone, please take your seats.");
+                    tell_room(ETP,"%^C144%^The professor shuffles his %^C193%^parchments %^C144%^around and waits.%^CRST%^\n");                    
+                break;
+                case(2):
+                    tell_room(ETP,"%^C075%^students slowly sit on %^C094%^benches %^C075%^and hush.%^CRST%^\n");
+		            force_me("say I am professor "+prof_name+" and we are learning about "+prof_type+ " today.");
+                break;
+                case(3):
+		            force_me("say Specifically we are learning about the influence in Port Serith.");
+                    tell_room(ETP,"\n");
+                break;
+                case(4):
+		            force_me("say When the church in Port Serith was reconsecrated. The ceremony called for faiths and deities to be invited in.");
+                    tell_room(ETP,"\n");
+                break;
+                case(5):
+		            force_me("say Those involved in Callamir, Kreysneothosies, Jarmilla and Kismet... But an interloper managed to invite Lord Shadow... Our sociologists have already identified changes to the city because of these deities.");
+                    tell_room(ETP,"\n");
+                break;
+                case(6):
+				    force_me("say thank you, no questions today, meet me for office hours later.");
+                    tell_room(ETP,"%^C152%^The professor gathers up his papers and steps away from the lectern.%^CRST%^\n");
+                    tell_room(ETP,"%^C243%^Another professor walks into the classroom%^CRST%^\n");
+					new(MOBS"professor.c")->move(ETO);
+                    go_home(TO);
+                break;
+            return;
+                }
+    num++;
+    call_out("gods_lesson_1",5,tp,num);
+    return;
+}
+
+void history_lesson_2(object tp,int num){
+            switch(num){
+                case(0):
+                    tell_room(ETP,"%^C152%^The professor steps up to the %^C059%^lectern%^C152%^.%^CRST%^\n");
+					force_me("pose %^C097%^standing at the lectern teaching%^CRST%^");
+                break;
+                case(1):
+		            force_me("say Settle down everyone, please take your seats.");
+                    tell_room(ETP,"%^C144%^The professor shuffles his %^C193%^parchments %^C144%^around and waits.%^CRST%^\n");                    
+                break;
+                case(2):
+                    tell_room(ETP,"%^C075%^students slowly sit on %^C094%^benches %^C075%^and hush.%^CRST%^\n");
+		            force_me("say I am professor "+prof_name+" and we are learning about "+prof_type+ " today.");
+                break;
+                case(3):
+		            force_me("say Specifically we are learning about the exodus of the refugees.");
+                    tell_room(ETP,"\n");
+                break;
+                case(4):
+		            force_me("say After the massacre of the night of betrayal, several thousand people were trapped in Port Serith and as the Voice had sealed the White keep those people were at the whims of the Hounds");
+                    tell_room(ETP,"%^C144%^you hear several students whisper 'fuck the Hounds' from the seats around you.%^CRST%^\n");    					
+                    tell_room(ETP,"\n");
+                break;
+                case(5):
+		            force_me("say In exchange for something the library provided the Hounds allowed several thousand leave on ten ships, which travelled to Torm. This of course led to the Act of the Veil, which we will cover in a future class. ");
+                    tell_room(ETP,"\n");
+                break;
+                case(6):
+				    force_me("say thank you, no questions today, meet me for office hours later.");
+                    tell_room(ETP,"%^C152%^The professor gathers up his papers and steps away from the lectern.%^CRST%^\n");
+                    tell_room(ETP,"%^C243%^Another professor walks into the classroom%^CRST%^\n");
+					new(MOBS"professor.c")->move(ETO);
+                    go_home(TO);
+                break;
+            return;
+                }
+    num++;
+    call_out("history_lesson_1",5,tp,num);
+    return;
+}
+
+void dragons_lesson_2(object tp,int num){
+            switch(num){
+                case(0):
+                    tell_room(ETP,"%^C152%^The professor steps up to the %^C059%^lectern%^C152%^.%^CRST%^\n");
+					force_me("pose %^C097%^standing at the lectern teaching%^CRST%^");
+                break;
+                case(1):
+		            force_me("say Settle down everyone, please take your seats.");
+                    tell_room(ETP,"%^C144%^The professor shuffles his %^C193%^parchments %^C144%^around and waits.%^CRST%^\n");                    
+                break;
+                case(2):
+                    tell_room(ETP,"%^C075%^students slowly sit on %^C094%^benches %^C075%^and hush.%^CRST%^\n");
+		            force_me("say I am professor "+prof_name+" and we are learning about "+prof_type+ " today.");
+                break;
+                case(3):
+		            force_me("say Specifically we are learning about the black dragon Vasha.");
+                    tell_room(ETP,"\n");
+                break;
+                case(4):
+		            force_me("say Vasha is a black dragon that was raised from an egg by a Mielikkian ranger.");
+                    tell_room(ETP,"\n");
+                break;
+                case(5):
+		            force_me("say Vasha, with the training and support of several champions of light, has become an ardent defender of this island and this library specifically. It is clear though that Vasha's need for vengeance against the Blue guides her more than love for us.");
+                    tell_room(ETP,"\n");
+                break;
+                case(6):
+				    force_me("say thank you, no questions today, meet me for office hours later.");
+                    tell_room(ETP,"%^C152%^The professor gathers up his papers and steps away from the lectern.%^CRST%^\n");
+                    tell_room(ETP,"%^C243%^Another professor walks into the classroom%^CRST%^\n");
+					new(MOBS"professor.c")->move(ETO);
+                    go_home(TO);				
+                break;
+            return;
+                }
+    num++;
+    call_out("dragons_lesson_1",5,tp,num);
     return;
 }
