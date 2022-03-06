c782304a62ff518dc3d2ccaf7f0a8d85d344f4c2
shadowgate <law@shadowgate.org>
2022-03-06 18:00:03 +0000

    Autocommit

---
+++ cmds/feats/b/_bred_for_war.c
@@ -0,0 +1,48 @@
+#include <std.h>
+#include <daemons.h>
+
+inherit FEAT;
+
+void create() {
+    ::create();
+    feat_type("permanent");
+    feat_category("Horsemanship");
+    feat_name("bred for war");
+    feat_prereq("Mounted combat");
+    feat_desc("Your mount is a paragon of war training, resulting in unmatched breeding for war. With this feat, your mount gains additional toughness, gaining your level * 2 additional max hit points and +5 damage resistance. Now your faithful steed can wade into the rigors of war without fear of those who stand below you.");
+    permanent(1);
+}
+
+int allow_shifted() { return 0; }
+
+int prerequisites(object ob){
+    if(!objectp(ob)) return 0;
+    if(!FEATS_D->has_feat(ob,"mounted combat")) {
+      dest_effect();
+      return 0;
+    }
+    return ::prerequisites(ob);
+}
+
+void execute_feat() {
+    ::execute_feat();
+    dest_effect();
+}
+
+void permanent_effects(object ob) {
+    ::permanent_effects(ob);
+    dest_effect();
+    return;
+}
+
+void reverse_permanent_effects(object ob) {
+    ::reverse_permanent_effects(ob);
+    dest_effect();
+    return;
+}
+
+void dest_effect() {
+    ::dest_effect();
+    remove_feat(TO);
+    return;
+}
