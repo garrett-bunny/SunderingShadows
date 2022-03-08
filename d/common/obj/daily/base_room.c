461b76594c237e40e0e67a7e320d3cdcafe4c21a
shadowgate <law@shadowgate.org>
2022-03-08 21:30:03 +0000

    Autocommit

--- d/common/obj/daily/base_room.c
+++ d/common/obj/daily/base_room.c
@@ -17,6 +17,7 @@ void create()
     set_property("light",2);
     set_property("no sticks",1);
     set_property("teleport proof", 1);
+    set_property("no teleport", 1);
     //set_no_clean(1);
 
     set_short("Plane of Shadows");
