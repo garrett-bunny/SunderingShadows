e74e2c981bea5fcbc74acb878aad4645586c70ec
shadowgate <law@shadowgate.org>
2022-03-20 15:30:02 +0000

    Autocommit

--- cmds/feats/s/_scribe.c
+++ cmds/feats/s/_scribe.c
@@ -11,7 +11,7 @@ void create() {
     feat_category("ArcaneSpellcraft");
     feat_name("scribe");
     feat_syntax("scribe SPELL_NAME [as level SPELL_LEVEL] [for NUM times]");
-    feat_prereq("Bard, Cleric, Druid, Inquisitor, Mage, Magus, Paladin, Ranger, Sorcerer, Oracle");
+    feat_prereq("Bard, Cleric, Druid, Inquisitor, Mage, Magus, Paladin, Ranger, Sorcerer, Oracle, Thief");
     feat_desc("This feat allows a character to use their knowledge of the arcane to scribe simple scrolls of spells they already know. Such scrolls are only good for a single use. You must have an empty parchment sheet in your inventory for scribe to work. It has to be parchment, other writing materials won't do.
 
 SPELL_NAME
