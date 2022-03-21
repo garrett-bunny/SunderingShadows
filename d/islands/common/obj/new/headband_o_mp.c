5c6910639251588f1ecc05641c1b01c340c07f84
shadowgate <law@shadowgate.org>
2022-03-21 15:00:02 +0000

    Autocommit

--- d/islands/common/obj/new/headband_o_mp.c
+++ d/islands/common/obj/new/headband_o_mp.c
@@ -7,7 +7,7 @@ void create()
     set_name("circlet of mental prowess");
     set_id(({ "circlet", "silver circlet", "mental circlet", "circlet of mental prowess" }));
     set_short("%^BOLD%^%^WHITE%^Circlet of %^CYAN%^M%^BLUE%^e%^CYAN%^nt%^BLUE%^a%^CYAN%^l Pr%^BLUE%^o%^CYAN%^w%^BLUE%^e%^CYAN%^ss%^RESET%^");
-    set_obvious_short("%^BOLD%^%^WHITE%^A s%^RESET%^i%^BOLD%^lv%^RESET%^e%^BOLD%^r circlet adorned with a %^BOLD%^%^BLUE%^bl%^RESET%^%^BLUE%^u%^BOLD%^%^BLUE%^e g%^RESET%^%^BLUE%^e%^BOLD%^%^BLUE%^m%^RESET%^");
+    set_obvious_short("%^BOLD%^%^WHITE%^s%^RESET%^i%^BOLD%^lv%^RESET%^e%^BOLD%^r circlet adorned with a %^BOLD%^%^BLUE%^bl%^RESET%^%^BLUE%^u%^BOLD%^%^BLUE%^e g%^RESET%^%^BLUE%^e%^BOLD%^%^BLUE%^m%^RESET%^");
     set_long("%^BOLD%^%^WHITE%^This simple s%^RESET%^i%^BOLD%^lv%^RESET%^e%^BOLD%^r circlet has a small %^BOLD%^%^BLUE%^bl%^RESET%^%^BLUE%^u%^BOLD%^%^BLUE%^e g%^RESET%^%^BLUE%^e%^BOLD%^%^BLUE%^m %^WHITE%^set so that when worn, the gemstone perches on the wearer's brow as a symbol the powerful inner eye.  Despite its simplicity, the circlet has obviously been wrought by the hand of a master jeweler.%^RESET%^");
     set_lore("%^BOLD%^%^WHITE%^These circlets of mental prowess were once a common sight upon the brows of the realm's spellcasters, but those crafted lately are merely pale imitations.  It is rumored that an even more powerful version, referred to as circlets of mental superiority, once existed.  However, no examples of it have been seen in over three thousand years.%^RESET%^");
     set_value(400000);
@@ -28,8 +28,8 @@ int wear_func(){
     player = environment(this_object());
     room = environment(player);
     
-    tell_object(player, "%^RESET%^%^BOLD%^The %^BLUE%^gem %^WHITE%^on the silver circlet begins to softly %^CYAN%^glow %^WHITE%^as you place it upon your brow.%^RESET%^");
-    tell_room(room, "%^RESET%^%^BOLD%^The %^BLUE%^gem %^WHITE%^on "+player->query_cap_name()+"%^RESET%^%^BOLD%^'s silver circlet begins to %^CYAN%^glow %^WHITE%^as "+player->query_subjective()+" places it upon "+player->query_possessive()+" brow.%^RESET%^", player);
+    tell_object(player, "%^RESET%^%^BOLD%^The %^BLUE%^gem %^WHITE%^on the "+query_obvious_short()+" begins to softly %^CYAN%^glow %^WHITE%^as you place it upon your brow.%^RESET%^");
+    tell_room(room, "%^RESET%^%^BOLD%^The %^BLUE%^gem %^WHITE%^on "+player->query_cap_name()+"%^RESET%^%^BOLD%^'s "+query_obvious_short()+" begins to %^CYAN%^glow %^WHITE%^as "+player->query_subjective()+" places it upon "+player->query_possessive()+" brow.%^RESET%^", player);
     return 1;
 }
 
@@ -38,8 +38,8 @@ int remove_func(){
     player = environment(this_object());
     room = environment(player);
     
-    tell_object(ETO, "%^RESET%^%^BOLD%^The %^BLUE%^gem %^WHITE%^on the silver circlet stops %^CYAN%^glowing %^WHITE%^as you remove it from your brow.%^RESET%^");
-    tell_room(EETO, "%^RESET%^%^BOLD%^The %^BLUE%^gem %^WHITE%^on "+player->query_cap_name()+"%^RESET%^%^BOLD%^'s silver circlet stops %^CYAN%^glowing %^WHITE%^as "+player->query_subjective()+" removes it from "+player->query_possessive()+" brow.%^RESET%^", player);
+    tell_object(ETO, "%^RESET%^%^BOLD%^The %^BLUE%^gem %^WHITE%^on the "+query_obvious_short()+" stops %^CYAN%^glowing %^WHITE%^as you remove it from your brow.%^RESET%^");
+    tell_room(EETO, "%^RESET%^%^BOLD%^The %^BLUE%^gem %^WHITE%^on "+player->query_cap_name()+"%^RESET%^%^BOLD%^'s "+query_obvious_short()+" stops %^CYAN%^glowing %^WHITE%^as "+player->query_subjective()+" removes it from "+player->query_possessive()+" brow.%^RESET%^", player);
     return 1;
 }
 
