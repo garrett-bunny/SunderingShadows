1c4156fa61f12b8b29405edf93920a2583024fdd
shadowgate <law@shadowgate.org>
2022-03-29 05:00:03 +0000

    Autocommit

--- d/islands/serakii/obj/bone_book_3.c
+++ d/islands/serakii/obj/bone_book_3.c
@@ -9,6 +9,6 @@ void create(){
    "-bou%^C088%^n%^C160%^d b%^C088%^o%^C160%^ok%^CRST%^");
    set_long("%^C088%^This small book is bound in %^C160%^fine red leather%^C088%^. On the cover, in %^C178%^gold filigree%^C088%^ is a symbol of a a %^C190%^parapet%^C088%^. The pages are mostly empty but some have writing you could >read<.%^CRST%^\n");
    set_weight(0);
-   set_read("%^C129%^~ * ~%^CRST%^\n%^C135%^The Hounds have proven useful so far. Such a distraction! All of the commotion around the conclave has allowed the outsider to breach the realm. %^CRST%^\n\n %^C129%^~ * ~%^CRST%^\n%^C147%^ .\n%^C147%^ %^CRST%^\n\n %^C129%^~ * ~%^CRST%^\n%^C135%^ .%^CRST%^\n");
+   set_read("%^C129%^~ * ~%^CRST%^\n%^C135%^The Hounds have proven useful so far. Such a distraction! All of the commotion around the conclave has allowed the outsider to breach the realm. %^CRST%^\n\n %^C129%^~ * ~%^CRST%^\n%^C147%^I do not know how the true leader of the Hounds continues to visit Xurorak. I cannot get anywhere close to the pit. I wonder if he was gifted some item.\n%^C147%^ %^CRST%^\n\n %^C129%^~ * ~%^CRST%^\n%^C135%^With the commander dead at the battle of the Library, whatever gift he had is gone. I have tried to cast a spell to move back to the battle in time, but something blocks me. If the pennants can be replaced, I can move to the battle on the night of betrayal and take Xurorak's gift from him.%^CRST%^\n");
    set_language("common");
 }
